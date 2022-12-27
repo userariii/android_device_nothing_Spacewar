11d5343b811f8fbd6422e9db5bfb89c272ec1b0f
NotHarshhaa <reddyharshhaa12@gmail.com>
2022-12-27 21:57:12 +0530

    Spacewar: Initial product definition for Spacewar
    Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 Project-Elixir
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_Spacewar.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_Spacewar-eng \
+    aosp_Spacewar-user \
+    aosp_Spacewar-userdebug
