5995ccb3a0311c4d10ed90c7f150947a2d9b5bd2
NotHarshhaa <reddyharshhaa12@gmail.com>
2023-01-11 23:29:56 +0530

    [TMP] Spacewar: Include vendorsetup.sh & remove.txt
    - As per jenkins req
    
    Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+# Vendor Tree
+git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_nothing_Spacewar.git -b tiramisu vendor/nothing/Spacewar
+
+# Kernel Tree
+git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_nothing_sm7325.git -b tiramisu kernel/nothing/sm7325
